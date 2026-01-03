d62475516a3470b7a848eae69c1ffd44e22d17dd
Juan camilo <61300748+bichozx@users.noreply.github.com>
2026-01-03 14:13:05 -0500

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,26 @@
+// import { Controller, Get } from '@nestjs/common';
+// import { AppService } from './app.service';
+
+// @Controller()
+// export class AppController {
+//   constructor(private readonly appService: AppService) {}
+
+//   @Get()
+//   getHello(): string {
+//     return this.appService.getHello();
+//   }
+// }
+
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  healthCheck() {
+    return {
+      status: 'ok',
+      service: 'Commission System API',
+      docs: '/api/docs',
+    };
   }
 }
