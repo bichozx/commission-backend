7de386ff89694a8acdc41474c689012ebc09ac09
Juan camilo <61300748+bichozx@users.noreply.github.com>
2026-01-03 14:49:17 -0500

    cambios en despliegue
    se elimina versel.json y el api index.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,26 +1,13 @@
-// import { Controller, Get } from '@nestjs/common';
-// import { AppService } from './app.service';
-
-// @Controller()
-// export class AppController {
-//   constructor(private readonly appService: AppService) {}
-
-//   @Get()
-//   getHello(): string {
-//     return this.appService.getHello();
-//   }
-// }
-
 import { Controller, Get } from '@nestjs/common';
+
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+  constructor(private readonly appService: AppService) {}
+
   @Get()
-  healthCheck() {
-    return {
-      status: 'ok',
-      service: 'Commission System API',
-      docs: '/api/docs',
-    };
+  getHello(): string {
+    return this.appService.getHello();
   }
 }
